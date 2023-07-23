f8238d1ee5a0f053da51ac17f8e46346ef5636de
Nathaniel Kemme Nash <nkemme54@gmail.com>
2023-07-23 14:04:09 -0400

    Fixed All redirect Again

---
+++ dist/assets/ProblemLabel.2ff808bf.js
@@ -0,0 +1,1 @@
+import{j as e,F as i,a as r}from"./jsx-runtime.29fd2640.js";function m({problem:l,folderName:s}){function a(c){}return e(i,{children:r("div",{class:"Problem",onClick:a,children:[e("label",{className:"name",children:l.Name}),e("label",{className:"difficulty",children:l.Difficulty})]})})}export{m as P};
